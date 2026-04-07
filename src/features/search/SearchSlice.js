89b262139b417348bac83d918f56225918ac02c6
Erick-WG <erickgitahi95@gmail.com>
2026-04-07 21:23:24 +0300

    Feat: add error handling for failed posts loading
    Improve ux by displaying errors when fetching posts data fails

--- src/features/search/SearchSlice.js
+++ src/features/search/SearchSlice.js
@@ -3,12 +3,12 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const fetchPosts = createAsyncThunk(
     "search/fetchPosts",
-    async (subreddit = "popular") => {
+    async (subreddit = "popular", thunkAPI) => {
       try {
         const response = await fetch(`https://www.reddit.com/r/${subreddit}.json`);
   
         if (!response.ok) {
-          throw new Error("Failed to fetch posts");
+            throw new Error("Failed to fetch posts");
         }
   
         const json = await response.json();
@@ -17,8 +17,12 @@ export const fetchPosts = createAsyncThunk(
   
         return posts
       } catch (error) {
-        console.error(error);
-        throw error;
+        // const rejection = |>
+        thunkAPI.rejectWithValue({
+            error: true,
+            message: error.message
+        });
+        // console.log(rejection)
       }
     }
   );
@@ -28,9 +32,11 @@ const SearchSlice = createSlice({
     initialState: {
         searchTerm: null,
         posts: [],
-        postsPending: false,
-        postsFetchError: false,
-        errorFetching: ''
+        postsPending: true,
+        postsFetchError: {
+            error: false,
+            message: ''
+        }
     },
     reducers: {
         setSearchTerm: (state, action) => {
@@ -43,31 +49,39 @@ const SearchSlice = createSlice({
     selectors: {
         selectSearchTerm: (state) => state.searchTerm,
         selectPosts: (state) => state.posts,
-        selectErrorFetching: (state) => state.errorFetching,
         selectFetchError: (state) => state.postsFetchError,
         selectFetchPending: (state) => state.postsPending
     },
     extraReducers: (builder) => {
         builder
             .addCase(fetchPosts.pending, (state)=>{
-                state.postsPending = true
-                state.postsFetchError = false
+                state.postsPending = true;
+                state.postsFetchError.error = false;
+                state.postsFetchError.message = '';
             })
             .addCase(fetchPosts.fulfilled, (state, action) =>{
+                // addressing offline states (temporary solution -> works perfectly atm :)
+                if(action.payload == undefined) {
+                    state.postsFetchError.error = true;
+                    state.postsFetchError.message = 'Failed to fetch posts, check your internet connection...';
+                    state.postsPending = false;
+                    return
+                }
                 state.posts = action.payload
-                state.postsPending = false
-                state.postsFetchError = false
+                state.postsPending = false;
+                state.postsFetchError.error = false;
+                state.postsFetchError.message = '';
             })
             .addCase(fetchPosts.rejected, (state, action) => {
-                state.postsFetchError = true
-                state.errorFetching = action.payload
+                state.postsFetchError.error = action.payload.error;
+                state.postsFetchError.message = action.payload.message;
             })
     }
 })
 
 
 export const { setSearchTerm, clearSearchTerm } = SearchSlice.actions
-export const { selectSearchTerm, selectPosts, selectErrorFetching, selectFetchError, selectFetchPending } = SearchSlice.selectors
+export const { selectSearchTerm, selectPosts, selectFetchError, selectFetchPending } = SearchSlice.selectors
 
 
 export default SearchSlice.reducer
